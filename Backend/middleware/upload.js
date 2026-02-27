b28e795eca85397dc8b85717dfe211d7b815fc2a
Muhammad751sheharyar <sheharyar117794@gmail.com>
2026-02-27 23:44:26 +0500

    project

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 
-const storage = multer.memoryStorage(); // file RAM me ayegi
+const storage = multer.memoryStorage(); // file RAM me 
 const upload = multer({ storage });
 
 module.exports = upload;
